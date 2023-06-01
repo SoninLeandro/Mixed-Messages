bdc7884958f1067e64ade09f89eae404959de719
Leandro Sonin <soninleandro@gmail.com>
2023-06-01 13:57:40 +0100

    Add input for displaying message

--- JS/mixedMessages.js
+++ JS/mixedMessages.js
@@ -1,8 +1,8 @@
 const messageFactory = {
     whoArr: ['Your brother', 'Michael Jackson', 'A stranger', 'Leo Messi', 'Bin Laden', "Your friend's mom", 'A Tesco employee'],
     whatArr: ['buy you a boat', 'make you a cake', 'build a spaceship', 'steal your jewlery', 'hack your bank account', 'perform leg surgery to you','get you a cat'],
-    howArr: ['on your birthday', 'tomorrow', 'on your last day', 'while you are sleeping', 'with Batman', 'on a motorbike', 'on the New Moon'],
-    mishitoMessArr: ["You're welcome", "I know it", "You own me £40", "This is 100% real", "A witch told me", "Cats know this stuff"]
+    howArr: ['on your birthday', 'tomorrow', 'on your last day', 'while you are sleeping', 'with Batman', 'on Christmas Day', 'on the New Moon'],
+    mishitoMessArr: ["You're welcome", "I know it", "You own me £40", "This is 100% real", "A witch told me", "Cats know this stuff", "Don't tell anyone"]
 }
 
 function mixedMessages () {
@@ -11,8 +11,8 @@ function mixedMessages () {
     let how = messageFactory.howArr[Math.floor(Math.random() * messageFactory.howArr.length)]
     let mishitoMessage = messageFactory.mishitoMessArr[Math.floor(Math.random() * messageFactory.mishitoMessArr.length)]
 
-    let randomMessage = `Mishito Meme says: ${who} will ${what}, ${how}. ${mishitoMessage}.`
+    let randomMessage = `${who} will ${what}, ${how}. ${mishitoMessage}.`
 
-    console.log(randomMessage);
+    document.getElementById("message").value = randomMessage;
 }
 
